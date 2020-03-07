7165494885d971bfe78fae26ca13df41e3b2cf9e
Victoria Goliber <vgoliber@dwavesys.com>
2020-03-07 10:18:31 +0900

    Added files for repo

---
+++ NPP.py
@@ -0,0 +1,39 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## ------- Set up our list of numbers -------
+S = [25, 7, 13, 31, 42, 17, 21, 10]
+
+## ------- Set up our QUBO dictionary -------
+
+# TODO:  Add code here to define your QUBO dictionary
+
+## ------- Run our QUBO on the QPU -------
+# TODO:  Choose QPU parameters
+chainstrength = 1
+numruns = 10
+
+# Run the QUBO on the solver from your config file
+from dwave.system import DWaveSampler, EmbeddingComposite
+
+sample_set = EmbeddingComposite(DWaveSampler()).sample_qubo(Q, chain_strength=chainstrength, num_reads=numruns)
+
+## ------- Return results to user -------
+R = iter(sample_set)
+E = iter(sample_set.data())
+for line in sample_set:
+    sample = next(R)
+    S1 = [S[i] for i in sample if sample[i] > 0]
+    S0 = [S[i] for i in sample if sample[i] < 1]
+    print("S0 Sum: ", sum(S0), "\tS1 Sum: ", sum(S1), "\t", S0)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # number-partitioning
 Number partitioning problem coding exercise.
+
+In this exercise, students are given an Ocean program outline and must create the QUBO dictionary (Q) that is provided to the D-Wave QPU and choose appropriate values for `chainstrength` and `numruns` on lines 24-25.
+
+The exercise is complete when you are able to see output showing S0 sum and S1 sum are both equal to 83.
